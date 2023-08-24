662bcbf090b73698facbadb8c7a15292c65e5bb7
Katya Miranda <katyamiranda03@hotmail.com>
2023-08-23 19:23:41 -0600

    agregue input, button y una lista de item más estilos

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola, Coder!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='item de la lista'
+        style={styles.input}>
+        </TextInput>
+        <Button title= "ADD" color={"#ff0000"}/>
+      </View>
+    
+      <View style={styles.listContainer}>
+        <View syles={styles.TextContainer}> 
+          <Text style={styles.Text}> Item 1 </Text>
+          </View>
+        <View syles={styles.TextContainer}> 
+          <Text style={styles.Text}> Item 2 </Text> 
+          </View>
+        <View syles={styles.TextContainer}>  
+          <Text style={styles.Text}> Item 3 </Text> 
+          </View>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: 'center',
+  },
+  input: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    width: 200,
+    height: 50,
+    fontSize: 20,
+    padding: 10,
+  },
+  listContainer:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
+  Text: {
+    fontSize: 24,
+  },
+
+  TextContainer:{
+    borderColor:"blue",
+    alignItems: "center",
+    borderWidth: 2,
+    marginVertical: 20,
+    padding: 10,
+    width: "100%",
+  }
 });
